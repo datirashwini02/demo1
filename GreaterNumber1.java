cf8aa01f9bd547fa902efc7688fd1f04a52a9654
datirashwini02 <101565404+datirashwini02@users.noreply.github.com>
2022-03-16 14:17:42 +0530

    Add files via upload

---
+++ GreaterNumber1.java
@@ -0,0 +1,22 @@
+package pack1;
+import java.util.Scanner;
+public class GreaterNumber1 {
+
+	public static void main(String[] args) {
+		
+		int a, b, c, largest;  
+		
+		Scanner sc = new Scanner(System.in);  
+		 
+		System.out.println("Enter the first number:");  
+		a = sc.nextInt();  
+		System.out.println("Enter the second number:");  
+		b = sc.nextInt();  
+		System.out.println("Enter the third number:");  
+		c = sc.nextInt();  
+		largest = c > (a > b ? a : b) ? c : ((a > b) ? a : b);  
+		System.out.println("The largest number is: "+largest);  
+		}  
+	}
+
+
